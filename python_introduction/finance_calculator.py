f4f213f6a122e044d29211bc65870f60763ef661
hadura <hardura5@gmail.com>
2025-11-09 08:45:42 +0100

    add finance calculator script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 monthly_income = float(input ("enter your monthly income?"))
 
-monthly_expenses = float(input ("enter your total monthly savings?"))
+total_monthly_expenses = float(input ("enter your total monthly savings?"))
 
 interest = 0.05 
-monthly_savings = monthly_income - monthly_expenses
+monthly_savings = monthly_income - total_monthly_expenses
 projected_annual_savings = (monthly_savings * 12) + (monthly_savings * 12 + 0.05)
 
 print ( f"your monthly savings are {monthly_savings}")
