c3689263e50b259af91b2217e21e5e58bfc71789
hadura <hardura5@gmail.com>
2025-11-10 00:30:49 +0100

    Add finance_calculator.py to calculate monthly and annual savings with 5% interest

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,14 @@
-monthly_income = int(input ("enter your monthly income:"))
 
-total_monthly_expenses = int(input ("enter your total monthly expenses?"))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-  monthly_savings = monthly_income - total_monthly_expenses
-  annual_interest_rate = 0.05
-projected_annual_savings = (monthly_savings * 12) + (monthly_savings * 12 + 0.05)
+
+monthly_savings = monthly_income - monthly_expenses
+
+
+annual_interest_rate = 0.05
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * annual_interest_rate)
+
 
 print(f"Monthly Savings: ${monthly_savings:.2f}")
 print(f"Projected Annual Savings (including 5% interest): ${projected_savings:.2f}")
