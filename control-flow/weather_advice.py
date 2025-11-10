30cc5c086b7ec50cf8e7cc8c7c937502d9dfeab3
hadura <hardura5@gmail.com>
2025-11-10 19:31:49 +0100

    Add weather recommendation script with clothing suggestions

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,10 @@
 weather = input("what's the weather like today? (sunny/rainy/cold):").lower()
 
 if weather == "sunny":
-    print ("wear a t-shrit and sunglasses.")
-
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print ("don't forget your umbrella and a raincoat.")
-
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-    print ("make sure to wear a warm coat and a scarf.")
-
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print ("sorry, i don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
